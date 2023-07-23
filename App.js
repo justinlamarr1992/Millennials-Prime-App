16791de52c72485c30e060150809cc3416fb3d46
JustAPPin LLC <justappinllc@gmail.com>
2023-07-23 14:29:30 -0500

    Learning TodoList

--- App.js
+++ App.js
@@ -8,36 +8,24 @@ import {
 import { useState } from "react";
 
 export default function App() {
-  const [people, setPeople] = useState([
-    { name: "shaun", id: "1" },
-    { name: "yoshi", id: "2" },
-    { name: "mario", id: "3" },
-    { name: "luigi", id: "4" },
-    { name: "peach", id: "5" },
-    { name: "toad", id: "6" },
-    { name: "bowser", id: "7" },
-    { name: "donkey kong", id: "8" },
-    { name: "diddy kong", id: "9" },
+  const [todos, setTodos] = useState([
+    { text: "Build App", key: "1" },
+    { text: "Build Website", key: "2" },
+    { text: "Fish for Clients", key: "3" },
   ]);
-
-  const pressHandler = (id) => {
-    console.log(id);
-    setPeople((prevPeople) => {
-      return prevPeople.filter((person) => person.id != id);
-    });
-  };
 
   return (
     <View style={styles.container}>
-      <FlatList
-        keyExtractor={(item) => item.id}
-        data={people}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-            <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
+      {/* Header */}
+      <View style={styles.content}>
+        {/* ToDo Form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => <Text>{item.text}</Text>}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -46,17 +34,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingTop: 40,
-    paddingHorizontal: 20,
+    // paddingTop: 40,
+    // paddingHorizontal: 20,
     // alignItems: "center",
     // justifyContent: "center",
-  },
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: "pink",
-    fontSize: 24,
-    marginHorizontal: 10,
-    marginTop: 24,
   },
 });
