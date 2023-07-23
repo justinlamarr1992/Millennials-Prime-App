984abb9724dac57143892f5a65f836f8ddb99ede
JustAPPin LLC <justappinllc@gmail.com>
2023-07-23 11:32:59 -0500

    Learning List

--- App.js
+++ App.js
@@ -1,34 +1,36 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ScrollView, SafeAreaView } from "react-native";
-import { Image } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import { useState } from "react";
-import { Stack, useRouter } from "expo-router";
-
-import { COLORS, icons, images, SIZES } from "./constants";
-// import {
-//   Nearbyjobs,
-//   Popularjobs,
-//   ScreenHeaderBtn,
-//   Welcome,
-// } from "./home/components/Welcome.jsx";
 
 export default function App() {
+  const [people, setPeople] = useState([
+    { name: "shaun", key: "1" },
+    { name: "yoshi", key: "2" },
+    { name: "mario", key: "3" },
+    { name: "luigi", key: "4" },
+    { name: "peach", key: "5" },
+    { name: "toad", key: "6" },
+    { name: "bowser", key: "7" },
+    { name: "donkey kong", key: "8" },
+    { name: "diddy kong", key: "9" },
+  ]);
+
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.priC }}>
-      {/* <Stack.Screen /> */}
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={{ flex: 1 }}>
-          {/* <View style={{ flex: 1, padding: SIZES.medium }}> */}
-          <Text>HELLO</Text>
-          {/* <Welcome /> */}
-          {/* <Image
-          source={require("./assets/images/MillennialsPrimeLogoNB.png")}
-          style={{ width: 250, height: 250 }}
-        /> */}
-          <StatusBar style="auto" />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.container}>
+      {/* Better for performance */}
+      <FlatList
+        data={people}
+        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+      />
+
+      {/* 
+      <ScrollView>
+        {people.map((item) => (
+          <View key={item.key}>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        ))}
+      </ScrollView> */}
+    </View>
   );
 }
 
@@ -36,7 +38,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: "pink",
+    fontSize: 24,
   },
 });
