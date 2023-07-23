a59e3456d701a729e6afeab2970696c92e2fab8e
JustAPPin LLC <justappinllc@gmail.com>
2023-07-23 11:37:38 -0500

    Learning List

--- App.js
+++ App.js
@@ -3,21 +3,24 @@ import { useState } from "react";
 
 export default function App() {
   const [people, setPeople] = useState([
-    { name: "shaun", key: "1" },
-    { name: "yoshi", key: "2" },
-    { name: "mario", key: "3" },
-    { name: "luigi", key: "4" },
-    { name: "peach", key: "5" },
-    { name: "toad", key: "6" },
-    { name: "bowser", key: "7" },
-    { name: "donkey kong", key: "8" },
-    { name: "diddy kong", key: "9" },
+    { name: "shaun", id: "1" },
+    { name: "yoshi", id: "2" },
+    { name: "mario", id: "3" },
+    { name: "luigi", id: "4" },
+    { name: "peach", id: "5" },
+    { name: "toad", id: "6" },
+    { name: "bowser", id: "7" },
+    { name: "donkey kong", id: "8" },
+    { name: "diddy kong", id: "9" },
   ]);
 
   return (
     <View style={styles.container}>
-      {/* Better for performance */}
+      {/* Better for performance and auto looks for key prop*/}
       <FlatList
+        // This is if your "key is another name"
+        keyExtractor={(item) => item.id}
+        // numColumns={2}
         data={people}
         renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
       />
@@ -48,5 +51,7 @@ const styles = StyleSheet.create({
     padding: 30,
     backgroundColor: "pink",
     fontSize: 24,
+    marginHorizontal: 10,
+    marginTop: 24,
   },
 });
